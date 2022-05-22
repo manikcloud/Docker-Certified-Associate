54cfa6dccf7536df951905577c3354524569865b
root <root@DESKTOP-7K0TCSQ.localdomain>
2022-05-22 15:02:07 +0800

    adding user data

---
+++ docker-installation.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+echo "# Uninstall old versions🔗"
+# Older versions of Docker were called docker, docker.io, or docker-engine. If these are installed, uninstall them:
+sleep 1
+sudo apt-get remove docker docker-engine docker.io containerd runc -y
+
+echo " Set up the repository"
+# Update the apt package index and install packages to allow apt to use a repository over HTTPS:
+sleep 1
+sudo apt-get update -y
+sudo apt-get install ca-certificates curl gnupg lsb-release -y
+
+echo "Use the following command to set up the stable repository. To add the nightly or test repository, add the word nightly or test (or both) after the word stable in the commands below."
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+echo "Install Docker Engine"
+# Update the apt package index, and install the latest version of Docker Engine, containerd, and Docker Compose, or go to the next step to install a specific version:
+sleep 1
+sudo apt-get update -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+
+systemctl start docker 
+# systemctl status docker 
+systemctl enable docker 
+
+docker info
+docker --version
---
+++ main.tf
@@ -0,0 +1,69 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
+
+
+resource "aws_security_group" "allow_SSH" {
+  name        = "allow_SSH_Docker"
+  description = "Allow SSH inbound traffic"
+  #   vpc_id      = aws_vpc.main.id
+
+
+  ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+    # description      = "SSH from VPC"
+    # from_port        = 22
+    # to_port          = 22
+    # protocol         = "tcp"
+    # cidr_blocks      = ["61.6.14.46/32"]
+    # # ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
+
+resource "aws_key_pair" "deployer1" {
+  key_name   = "deployer-key11"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDD8gTEP0wdqJnHkVDc7IMzFygpsibTemZylZvk4gcGiaa+YX8/VpUEyOrqYOg/8OrOAsdQCpPcc8J3AhUBSvgjeKc7sJZ0N8v+AOKnVQDni/y+9mWD6oGOszzH6DVfoHOy1HRZAbF6n+xFkk0DeisSJ3FsGVQK/J5rYgXVCBJii34mL0+DOBogxLva8tDcLgWLz+qxs8QcH4hgrHkdT8whBKfKcypICGS4U/WgXFW5M3pN4OynSzeqJ9gXn0Fbq50J/t6Cpnr6wqlzRzocAMXnlgrchYZWZJgTL3W8zrg1Fx7RBy3fAbEyWoyrkB9cFIPSkmiUZG9xUhirI4MbdgHVNlkp7xU0V6KfCRlTNE+xzWlbPSga7kjlqiizjDoR7xExKNLbNthB6nlNIEwX+FljtgrTqMV2w6k8H3xfrhdG0993EnMMwdPVmltEjJeZ+l3BETUcibKGFFc5iyR99W7NKhriH5d6OvbXev7JmbqsXrwn8rnu3auDGJs7YaIwzmk= varunmanikoutlo@ip-172-31-17-206"
+}
+
+####### Ubuntu VM #####
+
+
+resource "aws_instance" "ubuntu" {
+  ami                    = "ami-04505e74c0741db8d"
+  instance_type          = "t2.micro"
+  key_name               = aws_key_pair.deployer1.key_name
+  vpc_security_group_ids = ["${aws_security_group.allow_SSH.id}"]
+  tags = {
+    "Name" = "UBUNTU-Node"
+    "ENV"  = "Dev"
+  }
+  user_data = "${file("docker-installation.sh")}"
+
+  depends_on = [aws_key_pair.deployer1]
+
+}
+
